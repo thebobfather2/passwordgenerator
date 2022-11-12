572b81b6a141d1ce4c569c4ced73310a2c9a4891
Bryan Pike <bryanandrewpike@gmail.com>
2022-11-12 00:42:42 -0500

    removed switch method and applied a better user interface. Despite the better user interface, this method doesn't guarantee a character type because if a user selects a smaller length of password, the probability of it grabbing a character from each type is low. more work would be needed on this method to achieve acceptance criteria.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
   var passwordLength = 0;
   var passwordType = {
     lowercase: false,
@@ -30,109 +28,50 @@ function writePassword() {
       window.alert("Please enter a number between 8-128.")
       return
     }
-    // Select character types (lowercase, uppercase, numberic, and/or special charactrs)
-    var passwordCriterea = window.prompt("Using 1 for lowercase, 2 for uppercase, 3 for numeric, and 4 for special characters, please select your password criteria. NOTE: To select multiple critera, do NOT use spaces and list the criteria identifiers in order from least to greatest. For example, lowercase, uppercase, and special character would be entered as 124");
-    passwordCriterea = parseInt(passwordCriterea);
-
-    // Switch statement in dot notation
-    switch (passwordCriterea) {
-      case 1:
-        passwordType.lowercase = true;
-        break;
-      case 2:
-        passwordType.uppercase = true;
-        break;
-      case 3: 
-        passwordType.numeric = true;
-        break;
-      case 4:
-        passwordType.specialCharacters = true;
-        break;
-      case 12:
-        passwordType.lowercase = true;
-        passwordType.uppercase = true;
-        break;
-      case 13:
-        passwordType.lowercase = true;
-        passwordType.numeric = true;
-        break;
-      case 14:
-        passwordType.lowercase = true;
-        passwordType.specialCharacters = true;
-        break;
-      case 23:
-        passwordType.uppercase = true;
-        passwordType.numeric = true;
-        break;
-      case 24:
-        passwordType.uppercase = true;
-        passwordType.specialCharacters = true;
-        break;
-      case 34: 
-        passwordType.numeric = true;
-        passwordType.specialCharacters = true;
-        break;
-      case 123:
-        passwordType.lowercase = true;
-        passwordType.uppercase = true;
-        passwordType.numeric = true;
-        break;
-      case 124:
-        passwordType.lowercase = true;
-        passwordType.uppercase = true;
-        passwordType.specialCharacters = true;
-        break;
-      case 134:
-        passwordType.lowercase = true;
-        passwordType.numeric = true;
-        passwordType.specialCharacters = true;
-        break;
-      case 234:
-        passwordType.uppercase = true;
-        passwordType.numeric = true;
-        passwordType.specialCharacters = true;
-        break;
-      case 1234:
-        passwordType.lowercase = true;
-        passwordType.uppercase = true;
-        passwordType.numeric = true;
-        passwordType.specialCharacters = true;
-        break;
-      default:
-        window.alert("Invalid entry, please carefully read the instructions to select your criteria.")
-        break;
-    };
-    
-    // Add possible Character Types
-    if (passwordType.lowercase) {
-      passwordCharacters = "abcdefghijklmnopqrstuvwxyz";
-    };
-    if (passwordType.uppercase) {
-      passwordCharacters = passwordCharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    };
-    if (passwordType.numeric) {
-      passwordCharacters = passwordCharacters + "0123456789";
-    };
-    if (passwordType.specialCharacters) {
-      passwordCharacters = passwordCharacters + "!@#$%^&*?~";
-    };
-
-    // Variable to receive the length of the passwordCharacters
-    var passwordCharacterLength = passwordCharacters.length;
+    // confirm lowercase
+    var wantsLowercase = window.confirm ("Would you like your password to contain lowercase?");
+    //confirm uppercase
+    var wantsUppercase = window.confirm ("Would you like your password to contain uppercase?");
+    //confirm numeric
+    var wantsNumeric = window.confirm ("Would you like your password to contain numeric?");
+    //confirm special characters
+    var wantsSpecial = window.confirm ("Would you like your password to contain special characters?");
+    console.log (wantsLowercase, wantsUppercase, wantsNumeric, wantsSpecial);
+    return makePassword(wantsLowercase, wantsUppercase, wantsNumeric, wantsSpecial);
+  };
 
     // Randomly generate password
-    function makePassword () {
+    function makePassword (wantsLowercase, wantsUppercase, wantsNumeric, wantsSpecial) {
+
+    // Add possible Character Types
+    if (wantsLowercase) {
+      passwordCharacters += "abcdefghijklmnopqrstuvwxyz";
+    };
+    if (wantsUppercase) {
+      passwordCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    };
+    if (wantsNumeric) {
+      passwordCharacters += "0123456789";
+    };
+    if (wantsSpecial) {
+      passwordCharacters += "!@#$%^&*?~";
+    };
+
+    console.log(passwordCharacters)
+
       for (var i = 0; i < passwordLength; i++) {
-        passwordResults += passwordCharacters.charAt(Math.random() * (passwordCharacterLength - 1) + 1);
+        passwordResults += passwordCharacters[Math.floor(Math.random() * (passwordCharacters.length))];
       };
+      console.log(passwordResults)
       return passwordResults;
     }
     // Alert Message
-    window.alert("Generating Your Password...")
-    return makePassword();
-  };
+    //window.alert("Generating Your Password...")
+    //return makePassword();
+
 
   // Write password to the #password input
+  function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
